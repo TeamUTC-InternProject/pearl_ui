8455aef9c96a66f84b78a9dbfcfa0151274f6b27
Chris Evers <chrise92@vt.edu>
2018-07-25 18:39:35 -0400

    setup packaging structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pearl_ui',
+      version='0.1',
+      description='User interface for PEARL threat detection system',
+      url='https://github.com/TeamUTC-InternProject/pearl_ui',
+      author='Chris Evers',
+      author_email='chris.evers92@gmail.com',
+      license='MIT',
+      packages=['pearl_ui'],
+      zip_safe=False)
+
